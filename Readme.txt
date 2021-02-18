2b6217b987c161ba0e522340a74d9001f0a6d98b
Ratheshan <lionratheshan@gmail.com>
2021-02-18 09:56:15 +0530

    Second change

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
-readme
+Hi I'm Ratheshan :)
+
+This is an  simple multiplication Table
--- new.py
+++ new.py
@@ -1,4 +1,8 @@
+
+#this is simple multiplication table
+
 print("   Multipliactions   ")
+print()
 print(" 1 2 3 4 5 6 7 8 9 ")
 print('----------------------------')
 for i in range(1, 10):
